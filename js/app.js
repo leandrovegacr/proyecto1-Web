d1d35ad9acb5fe2d78205d83c179517c46831577
Leandro Vega <leandrovegacr@gmail.com>
2019-09-15 20:04:53 -0600

    Funcionando, con excepción del RegEx

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
-function ReservaPersonal(ident, entr, fechaRes) {
-    this.ident = ident;
-    this.entr = entr;
-    this.fechaRes = fechaRes;
+class ReservaPersonal {
+    constructor(ident, entr, fechaRes, pais, nombre) {
+        this.ident = ident;
+        this.entr = entr;
+        this.fechaRes = fechaRes;
+        this.pais = pais;
+        this.nombre = nombre;
+    }
 }
 
-function ReservaFecha(fechaRes, entr) {
-    this.fechaRes = fechaRes;
-    this.entr = entr;
+class ReservaFecha {
+    constructor(fechaRes, entr) {
+        this.fechaRes = fechaRes;
+        this.entr = entr;
+    }
 }
 
 var arregloFechas = [];
 
 var arregloPersonas = [];
 
-var getData = function () {
-    var patron = new RegExp('[1-9]{9}');
-
+var validacionDatos = function () {
     var pais = document.getElementById("pais").value;
     var id = document.getElementById("id").value;
     var nombre = document.getElementById("nombre").value;
@@ -24,32 +28,53 @@ var getData = function () {
     var fecha = document.getElementById("fecha").value;
     entradas = adultos + ninos;
 
-    var reservaPers = new ReservaPersonal(id, entradas, fecha);
+    var reservaPers = new ReservaPersonal(id, entradas, fecha, pais, nombre);
     var reservaFech = new ReservaFecha(fecha, entradas);
 
-    let pers = arregloPersonas.find(x=> (x.ident === id && x.fechaRes === fecha));
-    let indicePers = arregloPersonas.indexOf(x=> x.ident === id);
+    modificaCreaReservas(arregloFechas, arregloPersonas, reservaFech, reservaPers);
 
-    let fech = arregloFechas.find(y=> y.fechaRes === fecha);
-    let indiceFech = arregloFechas.indexOf(y=> y.fechaRes === fecha);
-
-    if (typeof pers=="undefined"){
-        arregloPersonas.push(reservaPers);
-    }
-    else{
-        pers.entr+=entradas;
-        arregloPersonas[indicePers]=pers;
-    }
-
-    if (typeof fech=="undefined"){
-        arregloFechas.push(reservaFech);
-    }
-    else{
-        fech.entr+=entradas;
-        arregloFechas[indiceFech]=fech;
-    }
-
-    console.log(arregloPersonas);
     console.log(arregloFechas);
+    console.log(arregloPersonas);
 }
+    
+function modificaCreaReservas(arregloF, arregloP, reservaF, reservaP) {
+    var validacionFecha = false;
+    var entradasF = 0;
+    var entradasP = 0;
+    var i;
+
+    i = arregloF.findIndex(x => x.fechaRes === reservaF.fechaRes);
+    if (i > -1){
+        entradasF = arregloF[i].entr;
+        entradasF += reservaF.entr;
+        if (entradasF<501){
+            arregloF[i].entr=entradasF;
+            validacionFecha = true;
+        }else{
+            alert("Lo sentimos, pero se ha sobrepasado la cantidad de entradas disponibles para este día");
+        }
+    }else{
+        arregloF.push(reservaF);
+        validacionFecha = true;
+    }
+    
+    if (validacionFecha) {
+        i = arregloP.findIndex(y => y.ident === reservaP.ident && y.fechaRes === reservaP.fechaRes);
+        if (i > -1){
+            entradasP = arregloP[i].entr;
+            entradasP += reservaP.entr;
+            if (entradasP<21){
+                arregloP[i].entr=entradasP;
+                alert("Su reserva fue realizada correctamente.");
+            }else{
+                alert("Solamente se pueden vender 20 entradas por día a una persona. Ud intenta sobrepasar el límite para este día");
+            }
+        }else{
+            arregloP.push(reservaP);
+            alert("Su reserva fue realizada correctamente.");
+        } 
+    }
+}
+
+
 
